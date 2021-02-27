52e0de59c83a4d54de5af4987b51cec74b05ce9f
Iftekhar Anam <iftekhar_anam@hotmail.com>
2021-02-27 20:15:52 +0600

    first commit

---
+++ functions/main.py
@@ -0,0 +1,30 @@
+'''
+Home work : 2
+Topic : Create Functions
+'''
+
+
+# Defing Functions
+def artist(artist):
+    result = artist
+    print(result)
+
+
+def title(title):
+    result = title
+    print(result)
+
+
+def year(year):
+    result = year
+    print(result)
+
+
+def songDeatils(title, artist, year=2016):
+    print(title)
+    print(artist)
+    print(year)
+
+
+# Calling the Functions
+songDeatils('I Feel it Coming', 'The Weekend')
---
+++ variables/main.py
@@ -0,0 +1,36 @@
+
+"""
+Home Work : 1
+Topic : Turn Song Meta into Variables and print them in Python
+"""
+
+# Song Meta as Variables
+song = 'I Feel it Coming'
+artist = 'Starboy'
+country = 'Canada'
+released = 'November 24, 2016'
+recorded = 2016
+studio = 'Gang Studio'
+genre = 'Disco'
+length = 4.29
+label = 'XORepublic'
+songWriter = 'Abel, TesfayeThomas, BangalterGuy-Manuel'
+producer = 'Daft PunkDoc McKinneyCirkutThe Weeknd (co.)'
+
+# Different Way
+one, two, three = 1, 2, 3
+
+# Printing All variables
+print(song)
+print(artist)
+print(country)
+print(released)
+print(recorded)
+print(genre)
+print(length)
+print(label)
+print(songWriter)
+print(producer)
+print(one)
+print(two)
+print(three)
